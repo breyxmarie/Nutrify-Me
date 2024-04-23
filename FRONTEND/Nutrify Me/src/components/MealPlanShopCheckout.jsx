615de87eee2d9ad4d128611e18862e8d01596203
breyxmarie <amcquiatchon@mymail.mapua.edu.ph>
2024-04-23 16:48:26 +0800

    k

--- FRONTEND/Nutrify Me/src/components/MealPlanShopCheckout.jsx
+++ FRONTEND/Nutrify Me/src/components/MealPlanShopCheckout.jsx
@@ -1,5 +1,13 @@
 import { useState, useRef } from "react";
-
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import * as React from "react";
 import MainUserNavbar from "./MainUserNavbar";
 import TeleMedNavBar from "./TeleMedNavBar";
@@ -21,7 +29,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import ReactDOM from "react-dom";
-import Modal from "@mui/material/Modal";
+import { Modal, Tab, Tabs } from "@mui/material";
 
 function MealPlanShopCheckout() {
   const { cartId } = useParams();
@@ -73,7 +81,7 @@ function MealPlanShopCheckout() {
     // Optionally, calculate and update shipping price based on selectedShipping here
   };
 
-  // modal codes
+  // modal codes open list address
   const style = {
     position: "absolute",
     top: "50%",
@@ -89,11 +97,161 @@ function MealPlanShopCheckout() {
     color: "#ffffff",
   };
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  // const [open, setOpen] = React.useState(false);
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
   //
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleTabChange = (event, newActiveTab) => {
+    setActiveTab(newActiveTab);
+  };
+
+  const tabContent = [
+    {
+      title: "Choose Address",
+      content: (
+        <div>
+          <br />
+          <Grid container spacing={2}>
+            <Grid xs={2}>
+              <img src="/images/location white.png" />
+            </Grid>
+            <Grid xs={8}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Address Selection
+              </Typography>
+            </Grid>
+            <Grid xs={2}>
+              <Button>
+                <img src="/images/close.png" />{" "}
+              </Button>
+            </Grid>
+          </Grid>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+
+          <FormControl sx={{ ml: 15, mb: 3 }}>
+            <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+            >
+              {listAddress.map((item, index) => (
+                <FormControlLabel
+                  value={item.address}
+                  control={<Radio />}
+                  label={(item.fullName, item.address)}
+                  // onChange=
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+          <center>
+            <Button
+              sx={{
+                background: "#ffffff",
+                color: "#E66253",
+                ml: 2,
+                height: "100%",
+                px: 2,
+                borderRadius: 5,
+                fontSize: "15px",
+                "&:hover": {
+                  backgroundColor: "#E66253",
+                  color: "#ffffff",
+                  border: 1,
+                },
+              }}
+            >
+              CHOOSE ADDRESS
+            </Button>
+          </center>
+        </div>
+      ),
+    },
+    {
+      title: "ADD ADDRESS",
+      content: (
+        <div>
+          <br />
+          <Grid container spacing={2}>
+            <Grid xs={2}>
+              <img src="/images/location white.png" />
+            </Grid>
+            <Grid xs={8}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Add New Address
+              </Typography>
+            </Grid>
+            <Grid xs={2}>
+              <Button>
+                <img src="/images/close.png" />{" "}
+              </Button>
+            </Grid>
+          </Grid>
+          <br />
+          <Grid container spacing={2}>
+            <Grid xs={6}>
+              Full Name <br />
+              <TextField
+                id="outlined-multiline-flexible"
+                sx={{ width: "100%", background: "#ffffff", borderRadius: 0 }}
+                placeholder="Name"
+                name="name"
+              />
+            </Grid>
+            <Grid xs={5} sx={{ ml: 4 }}>
+              Phone <br />
+              <TextField
+                id="outlined-multiline-flexible"
+                sx={{ width: "100%", background: "#ffffff", borderRadius: 0 }}
+                placeholder="number"
+                name="phone number"
+              />
+            </Grid>
+          </Grid>
+          <Typography>Address</Typography>
+
+          <TextField
+            id="outlined-multiline-flexible"
+            sx={{ width: "100%", background: "#ffffff", borderRadius: 0 }}
+            placeholder="Street Name, Building, House No."
+            name="address line 1"
+          />
+          <br />
+          <br />
+          <TextField
+            id="outlined-multiline-flexible"
+            sx={{ width: "100%", background: "#ffffff", borderRadius: 0 }}
+            placeholder="Barangay, City"
+            name="address line 2"
+          />
+          <br />
+          <br />
+          <TextField
+            id="outlined-multiline-flexible"
+            sx={{ width: "100%", background: "#ffffff", borderRadius: 0 }}
+            placeholder="Postal Code"
+            name="address line 3"
+          />
+        </div>
+      ),
+    },
+    // ... Add more tabs as needed
+  ];
   return (
     <div
       className="content"
@@ -111,14 +269,21 @@ function MealPlanShopCheckout() {
       </Typography>
 
       <Box sx={{ borderRadius: 0, border: 1, mx: 20 }}>
-        <Grid container spacing={2} sx={{ mt: "10px" }}>
+        <Grid container spacing={2} sx={{ my: "20px", mx: "20px" }}>
           <Grid xs={2}>
             <img src="/images/location.png" />
           </Grid>
-          <Grid xs={8}>
+          <Grid
+            xs={8}
+            sx={{
+              textAlign: "left",
+              color: "#99756E",
+              fontWeight: "bold",
+            }}
+          >
             Delivery Address
             {listAddress.slice(1).map((item, index) => (
-              <Typography>
+              <Typography sx={{ color: "#000000" }}>
                 {item.fullName} |{item.phoneNumber} <br />
                 {item.address}
               </Typography>
@@ -130,6 +295,44 @@ function MealPlanShopCheckout() {
             <Button onClick={handleOpen}>
               <img src="/images/right outline arrow.png" />
             </Button>
+            <Modal
+              open={isOpen}
+              onClose={handleClose}
+              aria-labelledby="modal-title"
+              aria-describedby="modal-description"
+            >
+              <Box sx={style}>
+                <Tabs
+                  value={activeTab}
+                  style={{
+                    color: "#f00", // Change text color to red
+                    fontSize: "18px", // Increase font size
+                    fontWeight: "bold", // Make text bold
+                  }}
+                  onChange={handleTabChange}
+                  indicatorColor="primary"
+                  centered
+                >
+                  {tabContent.map((tab, index) => (
+                    <Tab
+                      key={index}
+                      label={tab.title}
+                      style={{
+                        color: "#ffffff", // Change text color to red
+                        fontSize: "14px", // Increase font size
+                        //fontWeight: "bold", // Make text bold
+                      }}
+                    />
+                  ))}
+                </Tabs>
+                {tabContent.map((tab, index) => (
+                  <Box key={index} hidden={activeTab !== index}>
+                    {tab.content}
+                  </Box>
+                ))}
+              </Box>
+            </Modal>
+            {/* 
             <Modal
               open={open}
               onClose={handleClose}
@@ -179,8 +382,47 @@ function MealPlanShopCheckout() {
                     ))}
                   </RadioGroup>
                 </FormControl>
+                <center>
+                  <Button
+                    sx={{
+                      background: "#ffffff",
+                      color: "#E66253",
+                      ml: 2,
+                      height: "100%",
+                      px: 2,
+                      borderRadius: 5,
+                      fontSize: "15px",
+                      "&:hover": {
+                        backgroundColor: "#E66253",
+                        color: "#ffffff",
+                        border: 1,
+                      },
+                    }}
+                  >
+                    CHOOSE ADDRESS
+                  </Button>
+
+                  <Button
+                    sx={{
+                      background: "#ffffff",
+                      color: "#E66253",
+                      ml: 2,
+                      height: "100%",
+                      px: 2,
+                      borderRadius: 5,
+                      fontSize: "15px",
+                      "&:hover": {
+                        backgroundColor: "#E66253",
+                        color: "#ffffff",
+                        border: 1,
+                      },
+                    }}
+                  >
+                    ADD NEW ADDRESS
+                  </Button>
+                </center>
               </Box>
-            </Modal>
+            </Modal> */}
           </Grid>
         </Grid>
       </Box>
@@ -225,8 +467,10 @@ function MealPlanShopCheckout() {
 
       <br />
 
-      <Box sx={{ textAlign: "left", border: 1, mx: 20 }}>
-        <Typography sx={{ ml: 5, mt: 5 }}>PAYMENT OPTION </Typography>
+      <Box sx={{ textAlign: "left", border: 1, mx: 20, color: "#99756E" }}>
+        <Typography sx={{ ml: 5, mt: 5, color: "#99756E", fontWeight: "bold" }}>
+          PAYMENT OPTION{" "}
+        </Typography>
         <br />
         <FormControl sx={{ ml: 15, mb: 3 }}>
           <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
@@ -251,11 +495,15 @@ function MealPlanShopCheckout() {
       </Box>
       <br />
       <br />
-      <Box sx={{ border: 1, mx: 20 }}>
-        <Typography>PAYMENT DETAILS</Typography>
+      <Box sx={{ border: 1, mx: 20, color: "#99756E", fontSize: "20px" }}>
+        <Typography
+          sx={{ color: "#99756E", fontWeight: "bold", my: 5, fontSize: "30px" }}
+        >
+          PAYMENT DETAILS
+        </Typography>
         <hr />
         <br />
-        <Grid container spacing={2}>
+        <Grid container spacing={2} sx={{ my: 5 }}>
           {" "}
           <Grid xs={6}>ORDER SUBTOTAL</Grid>
           <Grid xs={6}>Php {subTotalPrices}</Grid>
@@ -263,7 +511,7 @@ function MealPlanShopCheckout() {
 
         <hr />
         <br />
-        <Grid container spacing={2}>
+        <Grid container spacing={2} sx={{ my: 5 }}>
           {" "}
           <Grid xs={6}>SHIPPING</Grid>
           <Grid xs={6}>
@@ -293,7 +541,7 @@ function MealPlanShopCheckout() {
         </Grid>
         <hr />
         <br />
-        <Grid container spacing={2}>
+        <Grid container spacing={2} sx={{ my: 5 }}>
           {" "}
           <Grid xs={6}>SHIPPING FEE</Grid>
           <Grid xs={6}>Php {shippingPrice}</Grid>
